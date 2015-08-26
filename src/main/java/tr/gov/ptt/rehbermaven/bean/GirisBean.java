17052bd248268a8bfa54b72dc32762791a6d1447
Administrator <Administrator@BEM-Bilgisayar>
2015-08-26 12:37:39 +0300

    Giriş bölümü düzeltildi. facade'a try-catch eklendi

--- src/main/java/tr/gov/ptt/rehbermaven/bean/GirisBean.java
+++ src/main/java/tr/gov/ptt/rehbermaven/bean/GirisBean.java
@@ -3,15 +3,19 @@ package tr.gov.ptt.rehbermaven.bean;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.NoResultException;
 import tr.gov.ptt.rehbermaven.entity.Giris;
 import tr.gov.ptt.rehbermaven.service.GirisService;
+import tr.gov.ptt.rehbermaven.util.JSFUtil;
 
 @Named(value = "girisBean")
 @RequestScoped
 public class GirisBean {
+
     private Giris giris = new Giris();
     @EJB
     private GirisService girisService;
+
     public GirisBean() {
     }
 
@@ -22,14 +26,16 @@ public class GirisBean {
     public void setGiris(Giris giris) {
         this.giris = giris;
     }
-    public String giriseYetkilimi()
-    {
+
+    public String giriseYetkilimi() {
         boolean sonuc = girisService.giriseYetkilimi(giris);
         if (sonuc) {
-            return "menu.xhtml";
+            return "menu.xhtml?faces-redirect=true";
         } else {
-            return "giris.xhtml";
+            JSFUtil.hataGoster("Hatalı Giriş", "Kullanıcı adı ya da şifre yanlış");
+            return "giris.xhtml?faces-redirect=true";
         }
+
     }
-    
+
 }
--- src/main/java/tr/gov/ptt/rehbermaven/facade/GirisFacade.java
+++ src/main/java/tr/gov/ptt/rehbermaven/facade/GirisFacade.java
@@ -7,6 +7,7 @@ package tr.gov.ptt.rehbermaven.facade;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import tr.gov.ptt.rehbermaven.entity.Giris;
 
@@ -16,6 +17,7 @@ import tr.gov.ptt.rehbermaven.entity.Giris;
  */
 @Stateless
 public class GirisFacade extends AbstractFacade<Giris> {
+
     @PersistenceContext(unitName = "rehbermavenPU")
     private EntityManager em;
 
@@ -27,15 +29,16 @@ public class GirisFacade extends AbstractFacade<Giris> {
     public GirisFacade() {
         super(Giris.class);
     }
-    public boolean girisKontrol(Giris p_giris)
-    {
-        Giris giris = em.createNamedQuery("Giris.girisKontrol", Giris.class)
-                .setParameter("kullanici", p_giris.getKullanici())
-                .setParameter("sifre", p_giris.getSifre())
-                .getSingleResult();
-        if(giris == null)
-            return false;
-        else
+
+    public boolean girisKontrol(Giris p_giris) {
+        try {
+            Giris giris = em.createNamedQuery("Giris.girisKontrol", Giris.class)
+                    .setParameter("kullanici", p_giris.getKullanici())
+                    .setParameter("sifre", p_giris.getSifre())
+                    .getSingleResult();
             return true;
+        } catch (NoResultException nre) {
+            return false;
+        }
     }
 }
